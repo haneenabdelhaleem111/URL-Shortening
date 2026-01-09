5d06ebaacb80fa552c8436fe7cda5a3b6eda857b
haneenabdelhaleem111 <haneen.abdelhaleem11@gmail.com>
2026-01-09 18:45:41 +0200

    Fix boost

--- src/Sections/BoostSection.js
+++ src/Sections/BoostSection.js
@@ -3,7 +3,8 @@ import React from "react";
 function BoostSection() {
   return (
     <section
-      className="mx-auto px-4 sm:px-8 py-12 bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"
+      className="mx-auto px-4 sm:px-8 py-12 bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center 
+ bg-[#3B3054]"
       style={{ backgroundImage: "url('images/bg-boost-desktop.svg')" }}
     >
       <h1 className="font-bold text-4xl sm:text-5xl py-8 text-white text-center">
