b62dc31b0e59d6e78e5768b328230457c6e1d548
haneenabdelhaleem111 <haneen.abdelhaleem11@gmail.com>
2026-01-09 18:41:41 +0200

    Fix boost

--- src/Sections/BoostSection.js
+++ src/Sections/BoostSection.js
@@ -3,15 +3,15 @@ import React from "react";
 function BoostSection() {
   return (
     <section
-      className=" mx-auto px-6 sm:px-10 py-16 bg-cover bg-center bg-no-repeat bg-[#3B3054] items-center justify-center"
+      className="mx-auto px-4 sm:px-8 py-12 bg-cover bg-center bg-no-repeat flex flex-col items-center justify-center"
       style={{ backgroundImage: "url('images/bg-boost-desktop.svg')" }}
     >
-      <h1 className="font-bold text-6xl py-12 text-white">
+      <h1 className="font-bold text-4xl sm:text-5xl py-8 text-white text-center">
         Boost your links today
       </h1>
       <button
         type="submit"
-        className="bg-[#2BCFCF] text-white text-xl font-bold px-12 py-6 rounded-full hover:bg-[#6FE1E1] transition"
+        className="bg-[#2BCFCF] text-white text-lg sm:text-xl font-bold px-8 py-4 rounded-full hover:bg-[#6FE1E1] transition"
       >
         Get Started!
       </button>
