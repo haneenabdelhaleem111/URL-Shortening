6f2dac82148e600288db00cd5b020769de5386cb
haneenabdelhaleem111 <haneen.abdelhaleem11@gmail.com>
2026-01-09 18:20:59 +0200

    Fix hero section

--- src/Sections/HeroSection.js
+++ src/Sections/HeroSection.js
@@ -2,18 +2,18 @@ import React from "react";
 
 function HeroSection() {
   return (
-    <section className="w-full py-36 lg:ps-56 pb-44">
-      <div className="flex flex-col-reverse lg:flex-row items-start gap-12 lg:gap-x-48 px-6 lg:px-16">
+    <section className="w-full py-28 lg:ps-44 pb-36">
+      <div className="flex flex-col-reverse lg:flex-row items-start gap-8 lg:gap-x-36 px-4 lg:px-12">
         {/* Text Content */}
-        <div className="lg:w-1/2 flex flex-col items-start py-32">
-          <h1 className="text-5xl sm:text-6xl lg:text-8xl font-bold leading-tight text-left">
+        <div className="lg:w-1/2 flex flex-col items-start py-24">
+          <h1 className="text-4xl sm:text-5xl lg:text-7xl font-bold leading-tight text-left">
             More than just shorter links
           </h1>
-          <p className="mt-7 text-lg sm:text-2xl lg:text-3xl text-gray-500 leading-relaxed text-left">
+          <p className="mt-5 text-base sm:text-xl lg:text-2xl text-gray-500 leading-relaxed text-left">
             Build your brand's recognition and get detailed insights on how your
             links are performing.
           </p>
-          <button className="mt-12 text-2xl bg-[#2BCFCF] text-white font-semibold px-8 py-4 rounded-full hover:bg-[#6FE1E1]">
+          <button className="mt-10 text-xl bg-[#2BCFCF] text-white font-semibold px-6 py-3 rounded-full hover:bg-[#6FE1E1]">
             Get Started
           </button>
         </div>
@@ -23,7 +23,7 @@ function HeroSection() {
           <img
             src="./images/illustration-working.svg"
             alt="working illustration"
-            className="w-full lg:max-w-5xl"
+            className="w-full lg:max-w-4xl"
           />
         </div>
       </div>
