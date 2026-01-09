a9617476b4513dc11d1c0891e08b3c38951b7228
haneenabdelhaleem111 <haneen.abdelhaleem11@gmail.com>
2026-01-09 18:36:39 +0200

    Fix stats

--- src/Components/StatsCard.js
+++ src/Components/StatsCard.js
@@ -1,12 +1,18 @@
 import React from "react";
+
 function StatsCard({ iconSrc, title, description, className }) {
   return (
-    <div className={`relative bg-white rounded-lg p-10 pt-24 ${className}`}>
-      <div className="absolute top-0 left-20 -translate-x-1/2 -translate-y-1/2 w-24 h-24 flex items-center justify-center rounded-full bg-[#3b3054]">
-        <img src={iconSrc} alt="icon" />
+    <div className={`relative bg-white rounded-lg p-6 pt-16 ${className}`}>
+      {/* Icon */}
+      <div className="absolute top-0 left-20 -translate-x-1/2 -translate-y-1/2 w-16 h-16 flex items-center justify-center rounded-full bg-[#3b3054]">
+        <img src={iconSrc} alt="icon" className="w-10 h-10" />
       </div>
-      <h1 className="text-4xl font-bold text-left mb-6">{title}</h1>
-      <p className="text-2xl font-semibold text-gray-400 text-left">
+
+      {/* Title */}
+      <h1 className="text-2xl sm:text-3xl font-bold text-left mb-4">{title}</h1>
+
+      {/* Description */}
+      <p className="text-base sm:text-lg font-semibold text-gray-400 text-left">
         {description}
       </p>
     </div>
